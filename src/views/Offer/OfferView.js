af1ef3921d751323e57c30c4e308076ec03b0427
pawel.jardzioch <p.jardzioch@gmail.com>
2018-01-24 14:21:47 +0100

    share button

--- src/views/Offer/OfferView.js
+++ src/views/Offer/OfferView.js
@@ -7,7 +7,7 @@ import Contact from './Contact';
 import { MapOfferContainer } from './MapOfferContainer';
 import InfoOfferView from  './InfoOfferView';
 import HeaderOffer from "./HeaderOffer";
-import DocMeta from 'react-doc-meta';
+import {Helmet} from "react-helmet";
 
 let testWeakMap = new WeakMap();
 
@@ -90,18 +90,17 @@ class OfferView extends Component {
                 </div>
             )
         }
-        const tags = [
-            {property: "og:title", content: "Title Here"},
-            {property: "og:type", content: "website"},
-            {property: "og:url", content: this.state.link},
-            {property: "og:image", content: this.state.picture},
-            {property: "og:description", content: this.state.description},
-            {property: "og:site_name", content: this.state.name},
-            {property: "fb:app_id", content: "1537301123055501"},
-        ];
         return (
             <div className="container">
-                <DocMeta tags={tags}/>
+                <Helmet>
+                    <meta property="og:title" content={this.state.name} data-doc-meta="true"/>
+                    <meta property="og:type" content="website" data-doc-meta="true"/>
+                    <meta property="og:url" content={this.state.link} data-doc-meta="true"/>
+                    <meta property="og:image" content={this.state.picture} data-doc-meta="true"/>
+                    <meta property="og:description" content={this.state.description} data-doc-meta="true"/>
+                    <meta property="og:site_name" content={this.state.name} data-doc-meta="true"/>
+                    <meta property="fb:app_id" content="1537301123055501" data-doc-meta="true"/>
+                </Helmet>
                 <div className='row'>
                     <div className='col-12 col-sm-12 col-md-12 col-lg-12' style={{backgroundColor: '#151b1e', margin: 0, padding: 0}} >
                         <div className='col-12 nopadding' style={{marginBottom: 25+'px'}}>
